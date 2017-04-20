f4ebe9af2b0eb3a93d75ac70e4a321ff99f46e66
Berdnikov Alexey <aberdnikov@gmail.com>
2017-04-20 09:01:34 +0500

    Update init.php

--- init.php
+++ init.php
@@ -1,6 +1,5 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-nestable')
     ->setSourceDir('public')
-    ->ngModule('ng-nestable')
     ->usePackage('larakit/sf-angular-larakit')
     ->jsPackage('angular-nestable.js');
